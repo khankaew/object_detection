b1a3d823793d43ea7d871fcf3a19ac879872cce2
Thiw <channanporn.kcs59@ubru.ac.th>
2021-09-28 23:04:53 +0700

    detect by canvas html

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let model;
 // declare a canvas variable and get its context
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+let canvasImg = document.getElementById("canvasImg");
+let ctxImg = canvasImg.getContext("2d");
 
 const setupCamera = () => {
   navigator.mediaDevices
@@ -28,7 +30,7 @@ const detectFaces = async () => {
   ctx.drawImage(video, 0, 0, 600, 400);
 
   prediction.forEach((pred) => {
-    
+
     // draw the rectangle enclosing the face
     ctx.beginPath();
     ctx.lineWidth = "4";
@@ -55,70 +57,79 @@ const detectFaces = async () => {
       heigth
     );
     ctx.stroke();
-    
+
     // drawing small rectangles for the face landmarks
     // ctx.fillStyle = "red";
     // pred.landmarks.forEach((landmark) => {
     //   ctx.fillRect(landmark[0], landmark[1], 5, 5);
     // });
-    
+
   });
 };
 
 let notFound = true
 const detectObject = async () => {
-  console.log('-- detectObject');
-  console.log(video);
-  const predictions = await model.detect(video)
-  // console.log(predictions);
+  // console.log('-- detectObject');
+  // console.log(video);
+  
+  drawCanvas()
+
+  const predictions = await model.detect(canvasImg)
+  console.log(predictions);
 
   // draw the video first
-  ctx.drawImage(video, 0, 0, 600, 400);
+  // ctx.drawImage(video, 0, 0, 600, 400);
 
-  for (var i = 0; i < predictions.length; i++) {
-    bbox = predictions[i].bbox;
+  // for (var i = 0; i < predictions.length; i++) {
+  //   bbox = predictions[i].bbox;
 
-    ctx.beginPath();
-    ctx.rect(bbox[0], bbox[1], bbox[2], bbox[3]);
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = "green";
-    ctx.stroke();
-    ctx.fillStyle = "green";
-    ctx.fillText(predictions[i].class, bbox[0] + 4, bbox[1] + 12);
+  //   ctx.beginPath();
+  //   ctx.rect(bbox[0], bbox[1], bbox[2], bbox[3]);
+  //   ctx.lineWidth = 3;
+  //   ctx.strokeStyle = "green";
+  //   ctx.stroke();
+  //   ctx.fillStyle = "green";
+  //   ctx.fillText(predictions[i].class, bbox[0] + 4, bbox[1] + 12);
 
-    const strengDetect = 20
-    const xStartCurrent = bbox[0]
-    const yStartCurrent = bbox[1]
-    const xStartOut = 80
-    const yStartOut = 80
-    const xEndOut = xStartOut + 204
-    const yEndOut = yStartOut + 326
-    const xStartIn = xStartOut + strengDetect
-    const yStartIn = yStartOut + strengDetect
-    const xEndIn = xStartIn + (204 - strengDetect * 2)
-    const yEndIn = yStartIn + (326 - strengDetect * 2)
-    const xCurrentEnd = xStartCurrent + bbox[2]
-    const yCurrentEnd = yStartCurrent + bbox[3]
+  //   const strengDetect = 20
+  //   const xStartCurrent = bbox[0]
+  //   const yStartCurrent = bbox[1]
+  //   const xStartOut = 80
+  //   const yStartOut = 80
+  //   const xEndOut = xStartOut + 204
+  //   const yEndOut = yStartOut + 326
+  //   const xStartIn = xStartOut + strengDetect
+  //   const yStartIn = yStartOut + strengDetect
+  //   const xEndIn = xStartIn + (204 - strengDetect * 2)
+  //   const yEndIn = yStartIn + (326 - strengDetect * 2)
+  //   const xCurrentEnd = xStartCurrent + bbox[2]
+  //   const yCurrentEnd = yStartCurrent + bbox[3]
 
-    console.log('Current', xStartCurrent, yStartCurrent);
-    console.log('Out', xStartOut, yStartOut);
-    console.log('Int', xStartIn, yStartIn);
+  //   console.log('Current', xStartCurrent, yStartCurrent);
+  //   console.log('Out', xStartOut, yStartOut);
+  //   console.log('Int', xStartIn, yStartIn);
 
-    if (((xStartOut < xStartCurrent) && (xStartCurrent < xStartIn))
-      && ((yStartOut < yStartCurrent) && (yStartCurrent < yStartIn)))
-    {
-      if (((xEndOut > xCurrentEnd) && (xCurrentEnd > xEndIn))
-        && ((yEndOut > yCurrentEnd) && (yCurrentEnd > yEndIn))) {
-          console.log('capping');
-          return null
-      }
-    }
-  }
+  //   if (((xStartOut < xStartCurrent) && (xStartCurrent < xStartIn))
+  //     && ((yStartOut < yStartCurrent) && (yStartCurrent < yStartIn))) {
+  //     if (((xEndOut > xCurrentEnd) && (xCurrentEnd > xEndIn))
+  //       && ((yEndOut > yCurrentEnd) && (yCurrentEnd > yEndIn))) {
+  //       console.log('capping');
+  //       return null
+  //     }
+  //   }
+  // }
 
   setTimeout(async () => {
     await detectObject();
   }, 40);
 };
+
+function drawCanvas() {
+  ctxImg.canvas.height = video.videoHeight;
+  ctxImg.canvas.width = video.videoWidth;
+
+  ctxImg.drawImage(video, 0, 0, canvasImg.width, canvasImg.height);
+}
 
 setupCamera();
 video.addEventListener("loadeddata", async () => {
